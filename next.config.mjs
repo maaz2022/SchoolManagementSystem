39ceae07cfba7eb339b1627c6e1467655ad111c9
maaz2022 <maaznazeer098@gmail.com>
2024-11-02 15:32:07 +0500

    ignore lint

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
+  eslint: {
+    // This option allows you to ignore ESLint issues during the build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
